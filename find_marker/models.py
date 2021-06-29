8644f974af00528ee28957d0ef31792b1673033b
sowish23 <soso2266@naver.com>
2021-06-29 14:33:17 +0900

    add address model

--- find_marker/models.py
+++ find_marker/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class ImageUploadModel(models.Model):
     document = models.ImageField(upload_to = 'images/%Y/%m/%d')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+class Address(models.Model):
+    document = models.ImageField(upload_to = 'images/%Y/%m/%d')
+    address = models.TextField()
