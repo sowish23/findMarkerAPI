f1cc08111422af6c1f887c00d66d81ce0b862959
sowish23 <soso2266@naver.com>
2021-07-02 09:55:36 +0900

    add media root url

--- find_marker/urls.py
+++ find_marker/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import postImg
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', postImg.as_view())
 ]
+
+urlpatterns += \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
