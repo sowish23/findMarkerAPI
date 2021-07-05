5b56c8a69277f2a75a458466e67166322e27d912
sowish23 <soso2266@naver.com>
2021-07-05 14:39:58 +0900

    edit models

--- find_marker/models.py
+++ find_marker/models.py
@@ -1,10 +1,21 @@
 from django.db import models
-
+from django.core.files import File
+import urllib
+import os
 
 class ImageUploadModel(models.Model):
-    document = models.ImageField(upload_to = 'images/%Y/%m/%d')
+    document = models.ImageField(upload_to = 'images', blank=True, null=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
+    # def get_remote_image(self):
+    #     if self.document_url and not self.document:
+    #         result = urllib.urlretrieve(self.document_url)
+    #         self.document.save(
+    #                 os.path.basename(self.document_url),
+    #                 File(open(result[0]))
+    #                 )
+    #         self.save()
+
 class Address(models.Model):
-    document = models.ImageField(upload_to = 'images/%Y/%m/%d')
+    document = models.ImageField(upload_to = 'images', blank=True, null=True)
     address = models.TextField(null = True)
