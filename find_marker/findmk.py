77e71923b55b9360bc4d80f18bbff3e2f782b87d
sowish23 <soso2266@naver.com>
2021-07-07 14:24:06 +0900

    add time sleep

--- find_marker/findmk.py
+++ find_marker/findmk.py
@@ -7,6 +7,7 @@ from PIL import Image
 import numpy as np
 from pytesseract import *
 import matplotlib.pyplot as plt 
+import time
 
 conn = sqlite3.connect("test.db", isolation_level=None, check_same_thread=False)
 
@@ -14,12 +15,11 @@ c = conn.cursor()
 
 def findmk(path):
     addr = ''
-    print(path)
+    print('findmk', path)
     img_original = plt.imread(path, cv2.IMREAD_COLOR)
-    print(type(img_original))
-    print(img_original)
 
     if (type(img_original) is np.ndarray):
+        time.sleep(1)
         img_resize = cv2.resize(img_original, dsize=(328, 207), interpolation=cv2.INTER_AREA)
         img_dia = img_resize[:180, :190]
         gray = cv2.cvtColor(img_dia, cv2.COLOR_BGR2GRAY)
@@ -36,11 +36,8 @@ def findmk(path):
             
             c.execute('SELECT * FROM shapeTable WHERE code='+str(vtc))
             rw = c.fetchone()
-            print('addr *^^* ', addr)
-            print('rw *^^* ', rw)
             if rw != None:
                 addr += rw[1]
-                print('addr *^^* ', addr)
 
         img_text = img_resize[:, 150:]
         text = pytesseract.image_to_string(img_text,lang='eng')
